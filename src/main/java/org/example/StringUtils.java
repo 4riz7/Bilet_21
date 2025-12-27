e259f141acda812856d7c0b52016590dc7a172f9
Иванов Вячеслав Павлович <227844@edu.fa.ru>
2025-12-27 13:09:50 +0300

    Create class StrinqUtils

--- src/main/java/org/example/StringUtils.java
+++ src/main/java/org/example/StringUtils.java
@@ -1,2 +1,12 @@
 package org.example;
 
+public class StringUtils {
+    public static int countDigits(String str) {
+        if (str == null) return 0;
+        int count = 0;
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) count++;
+        }
+        return count;
+    }
+}
