b1843c0693145ae1301b536d1c86e91d5661bd0d
Иванов Вячеслав Павлович <227844@edu.fa.ru>
2025-12-27 13:44:01 +0300

    End

--- src/main/java/org/example/StringUtils.java
+++ src/main/java/org/example/StringUtils.java
@@ -1,11 +1,21 @@
 package org.example;
 
 public class StringUtils {
+
     public static int countDigits(String str) {
-        if (str == null) return 0;
+        if (str == null) {
+            throw new IllegalArgumentException("Входная строка не может быть null");
+        }
+
+        if (str.equalsIgnoreCase("banan")) {
+            throw new IllegalArgumentException("Вводимое слово 'banan' запрещено");
+        }
+
         int count = 0;
         for (char c : str.toCharArray()) {
-            if (Character.isDigit(c)) count++;
+            if (Character.isDigit(c)) {
+                count++;
+            }
         }
         return count;
     }
